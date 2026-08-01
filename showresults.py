299a5525a41efdd702cee27bca26d47c45942a55
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-08-02 08:47:04 +1200

    Take top 5% for optimisation

--- showresults.py
+++ showresults.py
@@ -3,5 +3,16 @@ import matplotlib.pyplot as plt
 
 output = np.atleast_2d(np.loadtxt("output.txt"))
 
+# Objective function
+quality = np.sort(output[:,-1])
+
+# Take top 5%
+subset = output[:,-1] > quality[int(0.95*len(quality))]
+subset = output[subset, :]
+
+plt.figure(1)
 plt.plot(output[:,0], output[:,1], ".", markersize=1, alpha=0.2)
+
+plt.figure(2)
+plt.hist(subset[:,0], 100)
 plt.show()
